e8313c7794678d4fc8ba8fffe8691a1cc1579c86
Orlando Ferazzani <orli@Orlandos-MacBook-Air.local>
2024-08-06 19:51:32 +0200

    sidebar min 24:09

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -0,0 +1,17 @@
+import { OrgControl } from "./_components/OrgControl";
+
+
+const OrganizationIdLayout = ({
+    children
+}: {
+    children: React.ReactNode;
+}) => {
+    return (
+        <>
+            <OrgControl />
+            {children}
+        </>
+    )
+};
+
+export default OrganizationIdLayout; 
